26fb708c90cfe02f2f159315f7b9e786f05646bf
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-21 10:14:30 +0530

    React HW day 1

---
+++ HWDay1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeaderComp from './HeaderComp'
+function App() {
+  return (
+    <div>
+      <HeaderComp/>
+    </div>
+  )
+}
+
+export default App
---
+++ HWDay1/src/HeaderComp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HeaderComp() {
+    let show=()=>{
+        alert("Welcome to React Header")
+    }
+  return (
+    <div>
+        <h1>This is Header</h1>
+        <button onClick={show}>Click</button>
+    </div>
+  )
+}
+
+export default HeaderComp
