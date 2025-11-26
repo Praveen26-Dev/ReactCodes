85fa71ca339dec5bd127c13e7f41f305e53087c8
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-26 15:40:57 +0530

    Learning Tailwind in React

---
+++ Day5/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Tailwind.css'
+import Navbar from './Navbar'
+function App() {
+  return (
+    <div >
+      App
+    <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ Day5/src/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex justify-between p-5 bg-black h-15 text-white items-center text-xl ' > 
+        <div>
+             <h3>LOGO</h3>
+        </div>
+        
+        <div >
+            <ul className='flex  space-x-4'>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Profile</a>
+            <a href="">Careers</a>
+            </ul>
+        </div>
+        <div className='p-5'>
+           <button className=' border border-blue-200 rounded-3xl h-10 w-25 item-center bg-amber-500'>Download</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
