489f6c8579ca2bf3b24641bff98e5fcc2ad00fa7
Praveen26-Dev <praveenpandey709@gmail.com>
2025-12-09 10:46:24 +0530

    Started a PRoject TO Implement useState Hook

---
+++ LotteryProject/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Lottery from './Lottery'
+const App = () => {
+  return (
+  <>
+  <Lottery/>
+  {/* <Lottery/>
+  <Lottery/> */}
+  </>
+  )
+}
+
+export default App
